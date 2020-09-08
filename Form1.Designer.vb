cd87c15e67ea81c2d190810500a249cf11646b94
0000gordito0000@gmail.com <0000gordito0000@gmail.com>
2020-09-08 01:23:24 -0600

    texto botones

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -29,29 +29,29 @@ Partial Class Menu
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(293, 109)
+        Me.Button1.Location = New System.Drawing.Point(12, 23)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(214, 65)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "mensage 1"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(293, 193)
+        Me.Button2.Location = New System.Drawing.Point(257, 23)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(214, 65)
         Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "mensage 2"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(293, 286)
+        Me.Button3.Location = New System.Drawing.Point(493, 23)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(214, 65)
         Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
+        Me.Button3.Text = "mensage 3"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Menu
