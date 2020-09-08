366b5fafd97eab6b158c90f6c5b66ffbf29b5304
0000gordito0000@gmail.com <0000gordito0000@gmail.com>
2020-09-08 01:12:11 -0600

    codigo en los botones

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Menu
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("camnbios en el proyecto")
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("otro nuevo cambio")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox("ultimo cambio")
+    End Sub
 End Class
