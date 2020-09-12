9995c49a89ba25230aaf880917106b82688b3102
0000gordito0000@gmail.com <0000gordito0000@gmail.com>
2020-09-12 15:29:22 -0600

    888

--- Form1.vb
+++ Form1.vb
@@ -1,14 +1,14 @@
 ﻿Public Class Menu
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("sin cambios en el proyecto")
+        MsgBox("sin cambios dododo en el proyecto")
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        MsgBox("otro nuevo cambio")
+        MsgBox("otro nsnbsns nuevo cambio")
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        MsgBox("ultimo cambio")
+        MsgBox("ultimo rerere cambio")
         Me.Close()
     End Sub
 End Class
